d18caa1e47090f62a6f8a00b33d4e57446c716aa
NerdyPepper <akshayoppiliappan@gmail.com>
2018-02-01 22:26:06 +0530

    Clean up curl output, send upload status

--- lien.py
+++ lien.py
@@ -9,11 +9,11 @@ home = expanduser("~")
 for i in range(len(sys.argv) - 1):
 	curfile = 'file=@%s' %(sys.argv[i + 1])
 
-	with subprocess.Popen(["curl","-F", curfile,"https://0x0.st"], stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
+	with subprocess.Popen(["curl", "-s", "-F", curfile, "https://0x0.st"], stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
 		for line in p.stdout:
 			with open(home + '/scripts/logs.txt', 'at') as f:
 				f.write(sys.argv[i + 1])
 				f.write(' - ')
 				f.write(str(line))
-	notification = "notify-send 'Uploaded' '%s'" %(sys.argv[i+ 1])
 	subprocess.Popen(["notify-send", "Uploaded", (sys.argv[i + 1])])
+	subprocess.Popen(["echo", "Uploaded:", sys.argv[i + 1]])
