e4f80f06d90de9b35fbfbd8ce0ccb1312affe7c3
NerdyPepper <akshayoppiliappan@gmail.com>
2018-02-03 14:49:32 +0530

    Remove unwanted imports

--- lien.py
+++ lien.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3.5
 
 import sys
-import subprocess
+from  subprocess import Popen, PIPE
 
 for i in range(len(sys.argv) - 1):
 	curfile = 'file=@%s' %(sys.argv[i + 1])
-	with subprocess.Popen(["curl", "-s", "-F", curfile, "https://0x0.st"], stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as x:
+	with Popen(["curl", "-s", "-F", curfile, "https://0x0.st"], stdout=PIPE, bufsize=1, universal_newlines=True) as x:
 		for line in x.stdout:
 			with open('logs.txt', 'at') as f:
 				f.write(sys.argv[i + 1])
 				f.write(' - ')
 				f.write(str(line))
 			print("Uploaded %s" %(str(line)), end='')
-	subprocess.Popen(["notify-send", "Uploaded", (sys.argv[i + 1])])
+	Popen(["notify-send", "Uploaded", (sys.argv[i + 1])])
