11b2c1f71cbd36189a99f30eca3effd717b63f7e
NerdyPepper <akshayoppiliappan@gmail.com>
2018-02-12 17:08:03 +0530

    Fix path

--- lien.py
+++ lien.py
@@ -3,11 +3,12 @@
 import sys
 from  subprocess import Popen, PIPE
 
+
 for i in range(len(sys.argv) - 1):
 	curfile = 'file=@%s' %(sys.argv[i + 1])
 	with Popen(["curl", "-s", "-F", curfile, "https://0x0.st"], stdout=PIPE, bufsize=1, universal_newlines=True) as x:
 		for line in x.stdout:
-			with open('logs.txt', 'at') as f:
+			with open('/home/icyphox/lien/logs.txt', 'at') as f:
 				f.write(sys.argv[i + 1])
 				f.write(' - ')
 				f.write(str(line))
